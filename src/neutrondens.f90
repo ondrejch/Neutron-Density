e5c5b2f2c3af66a98b0489ccaadd1d4e20622bcc
Dallas Moser <dmoser63@yahoo.com>
2015-12-16 17:13:35 -0500

    fixed dfdt and fyt

--- src/neutrondens.f90
+++ src/neutrondens.f90
@@ -2,65 +2,70 @@ module neudens
 use iso_fortran_env
 use inputinterp
 implicit none
+real(real64) :: beta(7)      ! beta values for each decay group
+real(real64) :: lambda(6)    ! half life constants
+real(real64) :: pt           ! reactivity value
+real(real64) :: ngen         ! neutron generation time
 
 contains
 
 subroutine neuden()
-integer                          :: i               ! counting variables
-integer                          :: counter         ! iteration counter
-integer                          :: info            ! llapack error variable
-real(real64)                     :: h               ! step size
-real(real64), dimension(7)       :: y               ! matrix containing n(t) and c(t)
-real(real64), dimension(7)       :: y0              ! matrix containing initial values for n(t) and c(t)
-real(real64), dimension(7)       :: yscale          ! truncation error scaling value
-real(real64), dimension(7)       :: g1              ! variable of first equation
-real(real64), dimension(7)       :: g2              ! variable of second equation
-real(real64), dimension(7)       :: g3              ! variable of fourth equation
-real(real64), dimension(7)       :: g4              ! variable of fifth equation
-real(real64), dimension(7)       :: dfdt         
-real(real64), dimension(7)       :: fyt
-real(real64), dimension(7)       :: RHS1            ! right-hand-side of equation 1
-real(real64), dimension(7)       :: RHS2            ! right-hand-side of equation 2
-real(real64), dimension(7)       :: RHS3            ! right-hand-side of equation 3
-real(real64), dimension(7)       :: RHS4            ! right-hand-side of equation 4
-real(real64)                     :: nt              ! neutron density
-real(real64)                     :: pt              ! reactivity value
-real(real64)                     :: t               ! time
-real(real64), dimension(6)       :: Ct              ! delayed neutron precursers
-real(real64), dimension(7,7)     :: identity        ! identity matrix 
-real(real64), dimension(7,7)     :: LHS             ! left-hand-side of all linear equations
-real(real64), dimension(7,7)     :: dfdy
-real(real64), dimension(7)       :: beta            ! beta values for each decay group
-real(real64), dimension(7)       :: ipiv            ! pivot vector used in llapack subroutines
-real(real64), dimension(6)       :: lambda          ! half life constants
-real(real64)                     :: ngen            ! neutron generation time
-real(real64), parameter          :: gma = 0.5_real64        
-real(real64), parameter          :: a21 = 2.0_real64
-real(real64), parameter          :: a31 = 1.92_real64
-real(real64), parameter          :: a32 = 0.24_real64
-real(real64), parameter          :: c21 = -8.0_real64
-real(real64), parameter          :: c31 = 14.88_real64
-real(real64), parameter          :: c32 = 2.4_real64
-real(real64), parameter          :: c41 = -0.869_real64
-real(real64), parameter          :: c42 = -0.432_real64
-real(real64), parameter          :: c43 = -0.4_real64
-real(real64), parameter          :: b1 = 19.0_real64/9.0_real64
-real(real64), parameter          :: b2 = 0.5_real64
-real(real64), parameter          :: b3 = 25.0_real64/108.0_real64
-real(real64), parameter          :: b4 = 125.0_real64/108.0_real64
-real(real64), parameter          :: e1 = 17.0_real64/54.0_real64
-real(real64), parameter          :: e2 = 7.0_real64/36.0_real64
-real(real64), parameter          :: e3 = 0.0_real64
-real(real64), parameter          :: e4 = 125.0_real64/108.0_real64
-real(real64), parameter          :: c1 = 0.5_real64
-real(real64), parameter          :: c2 = -1.5_real64
-real(real64), parameter          :: c3 = 2.42_real64
-real(real64), parameter          :: c4 = 0.116_real64
-!real(real64), parameter          :: a2 = 1.0_real64
-!real(real64), parameter          :: a3 = 0.6_real64
-real(real64), dimension(7)       :: err
-real(real64), parameter          :: eps = 1E-2_real64       ! accepted error value
-real(real64)                     :: hretry, hnext, havg, errmax
+integer                 :: i                    ! counting variables
+integer                 :: counter              ! iteration counter
+integer                 :: info                 ! llapack error variable
+real(real64)            :: h                    ! step size
+real(real64)            :: y(7)                 ! matrix containing n(t) and c(t)
+real(real64)            :: y0(7)                ! matrix containing initial values for n(t) and c(t)
+real(real64)            :: yscale(7)            ! truncation error scaling value
+real(real64)            :: g1(7)                ! variable of first equation
+real(real64)            :: g2(7)                ! variable of second equation
+real(real64)            :: g3(7)                ! variable of fourth equation
+real(real64)            :: g4(7)                ! variable of fifth equation
+real(real64)            :: dfdt(7)         
+real(real64)            :: fyt(7)
+real(real64)            :: RHS1(7)              ! right-hand-side of equation 1
+real(real64)            :: RHS2(7)              ! right-hand-side of equation 2
+real(real64)            :: RHS3(7)              ! right-hand-side of equation 3
+real(real64)            :: RHS4(7)              ! right-hand-side of equation 4
+real(real64)            :: nt                   ! neutron density
+real(real64)            :: ptprev               ! reactivity from the previous iteration
+real(real64)            :: t                    ! time
+real(real64)            :: Ct(6)                ! delayed neutron precursers
+real(real64)            :: identity(7,7)        ! identity matrix 
+real(real64)            :: LHS(7,7)             ! left-hand-side of all linear equations
+real(real64)            :: dfdy(7,7)
+real(real64)            :: ipiv(7)              ! pivot vector used in llapack subroutines
+real(real64), parameter :: gma = 0.5_real64        
+real(real64), parameter :: a21 = 2.0_real64
+real(real64), parameter :: a31 = 1.92_real64
+real(real64), parameter :: a32 = 0.24_real64
+real(real64), parameter :: c21 = -8.0_real64
+real(real64), parameter :: c31 = 14.88_real64
+real(real64), parameter :: c32 = 2.4_real64
+real(real64), parameter :: c41 = -0.869_real64
+real(real64), parameter :: c42 = -0.432_real64
+real(real64), parameter :: c43 = -0.4_real64
+real(real64), parameter :: b1 = 19.0_real64/9.0_real64
+real(real64), parameter :: b2 = 0.5_real64
+real(real64), parameter :: b3 = 25.0_real64/108.0_real64
+real(real64), parameter :: b4 = 125.0_real64/108.0_real64
+real(real64), parameter :: e1 = 17.0_real64/54.0_real64
+real(real64), parameter :: e2 = 7.0_real64/36.0_real64
+real(real64), parameter :: e3 = 0.0_real64
+real(real64), parameter :: e4 = 125.0_real64/108.0_real64
+real(real64), parameter :: c1 = 0.5_real64
+real(real64), parameter :: c2 = -1.5_real64
+real(real64), parameter :: c3 = 2.42_real64
+real(real64), parameter :: c4 = 0.116_real64
+real(real64), parameter :: a2 = 1.0_real64
+real(real64), parameter :: a3 = 0.6_real64
+real(real64)            :: err(7)
+real(real64), parameter :: eps = 1E-2_real64       ! accepted error value
+real(real64)            :: hretry
+real(real64)            :: hnext
+real(real64)            :: havg
+real(real64)            :: errmax
+
 external dgetrf, dgetrs
  
 
@@ -97,7 +102,7 @@ else
   lambda(4) = 0.331_real64     ! decay constant of group 4
   lambda(5) = 1.26_real64      ! decay constant of group 5
   lambda(6) = 3.21_real64      ! decay constant of group 6
-  ngen      = 1E-7_real64   ! average neutron generation time  
+  ngen      = 1E-7_real64      ! average neutron generation time  
 end if
 
 ! initial values for nt and ct
@@ -150,13 +155,17 @@ do  ! Main loop
   end do
   
   ! Build vector fyt
-  fyt = matmul(dfdy, y)
-
+!!!  fyt = matmul(dfdy, y) !!!
+  fyt = get_fyt(y,t)
   ! Calculate yscale
   yscale = abs(y) + abs(h * fyt) + 1E-30_real64
  
   ! create matrix dfdt
-  dfdt(1) = (y(1)/ngen)*(0.0_real64) ! for non-constant rho dfdt = (nt/ngen)(dpt/dt) ! FIXME !
+  if (counter == 1) then
+    dfdt(1) = (y(1)/ngen)*(pt)
+  else
+    dfdt(1) = (y(1)/ngen)*(pt - ptprev)
+  end if  
   dfdt(2:7) = 0.0_real64
       
   ! start building left hand side matrix
@@ -183,19 +192,19 @@ do  ! Main loop
   g1 = RHS1 
   
   ! Solve for g2
-  fyt = matmul(dfdy,(y + a21*g1))
+  fyt = get_fyt((y + a21*g1), (t + a2*h))
   RHS2 = fyt + h*c2*dfdt + (c21*g1)/h
   call dgetrs('N', 7, 1, LHS, 7, ipiv, RHS2, 7, info)
   g2 = RHS2
 
   ! Solve for g3
-  fyt = matmul(dfdy,(y + a31*g1 + a32*g2))
+  fyt = get_fyt((y + a31*g1 + a32*g2), (t + a3*h))
   RHS3 = fyt + h*c3*dfdt + (c31*g1 + c32*g2)/h
   call dgetrs('N', 7, 1, LHS, 7, ipiv, RHS3, 7, info)
   g3 = RHS3
 
   ! Solve for g4
-  fyt = matmul(dfdy,(y + a31*g1 + a32*g2))
+  fyt = get_fyt((y + a31*g1 + a32*g2), (t + a3*h))
   RHS4 = fyt + h*c4*dfdt + (c41*g1 + c42* g2 + c43*g3)/h
   call dgetrs('N', 7, 1, LHS, 7, ipiv, RHS4, 7, info)
   g4 = RHS4
@@ -204,12 +213,9 @@ do  ! Main loop
 ! Automatic step size calculation
   err = e1*g1+e2*g2+e3*g3+e4*g4
   errmax = maxval(abs(err/yscale))
-!  print *, "errmax =", err
-!  print *, "yscale =", yscale
    if (errmax > eps) then
      hretry = max(0.9_real64*h/(errmax**(1.0/3.0)),0.5_real64*h) ! note that x^-a == 1/x^a
      h = hretry
-!     print *, h
      cycle
    else
      if (errmax>0.1296) then
@@ -238,13 +244,16 @@ do  ! Main loop
 
 
   havg = (havg + h)/counter
-!  print *," counter =", counter, "reactivity =", get_reactivity(t), "step = ", h
   counter = counter + 1
 
   if (t >= get_end_time()) then
     write(6, *)"havg = ", havg 
-    exit ! exit the do loop
+    exit ! exit main do loop
   end if
+  
+  ! save previous reactivity
+  ptprev = get_reactivity(t)
+
   ! find next time value  
   t = t + h
   
@@ -254,5 +263,29 @@ close(50)
 close(60)
 
 end subroutine neuden
+
+function get_fyt(y_in,t_in)
+! function to calculate the value of fyt
+  real(real64), intent(in) :: y_in(7)
+  real(real64), intent(in) :: t_in 
+  real(real64)             :: df(7,7)
+  real(real64)             :: get_fyt(7)
+  integer                  :: i  
+  pt = get_reactivity(t_in) 
+  df(1,1) = (pt - beta(7))/ngen
+  do i = 2,7
+    df(i,1) = beta(i-1)/ngen
+    df(1,i) = lambda(i-1)
+  end do
+
+  df(2:7,2:7)  = 0.0_real64
+  do i = 2,7
+    df(i,i) = -lambda(i-1)
+  end do
+
+  get_fyt = matmul(df,y_in)
+end function get_fyt
+
+
 
 end module neudens
